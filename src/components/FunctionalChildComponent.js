28eb9b3a6446ad34404df01fa7d0ec0cbff3ba77
JCH4905 <joseph_c_hurley@homedepot.com>
2018-05-21 10:23:05 -0400

    Added child components

---
+++ src/components/FunctionalChildComponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FunctionalChildComponent = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default FunctionalChildComponent;
---
+++ src/components/StatefulChildComponent.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class StatefulChildComponent extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default StatefulChildComponent;
